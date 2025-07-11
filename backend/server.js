e21add7b3c6975a8b9a060c7d07d1e002e601ff5
RafaelMEA <rafaelmartineaquino@gmail.com>
2025-07-11 20:16:58 +0800

    for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const apiKeyAuth = require("./middlewares/apiKeyAuth");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -9,11 +10,13 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
-// Apply API key authentication to all routes
-app.use(apiKeyAuth);
+app.use("/api", apiKeyAuth);
 
 const categoryRoutes = require("./routes/categoryRoutes");
 app.use("/api/categories", categoryRoutes);
+
+const distPath = path.join(__dirname, "../frontend/dist/");
+app.use(express.static(distPath));
 
 
 app.listen(PORT, () => {
